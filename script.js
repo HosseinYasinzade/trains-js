0eb32f633e42f7e8bc9df887a925de1d1f5c790b
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-10-29 23:33:02 +0330

    add train 2

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 // Creating a Shopping Cart
 
-const cartItems = [
-  { name: "Shoes", quality: 2, price: 75 },
-  { name: "Hat", price: 20 },
-  { name: "Socks", quality: 3 },
+// const cartItems = [
+//   { name: "Shoes", quality: 2, price: 75 },
+//   { name: "Hat", price: 20 },
+//   { name: "Socks", quality: 3 },
+// ];
+
+// const calcAll = () => {
+//   return cartItems.reduce((total, { price = 0, discount = 0 }) => {
+//     const finalPrice = price - (discount ?? 0);
+//     return total + finalPrice;
+//   }, 0);
+// };
+
+// const totalPrice = calcAll(cartItems);
+// console.log(`Total Price: $${totalPrice}`);
+
+// Nullish Coalescing with Product Prices
+
+const products = [
+  { name: "Book", price: 20 },
+  { name: "Pen", price: null },
+  { name: "Notebook", price: undefined },
+  { name: "Backpack" },
 ];
 
-const calcAll = () => {
-  return cartItems.reduce((total, { price = 0, discount = 0 }) => {
-    const finalPrice = price - (discount ?? 0);
-    return total + finalPrice;
-  }, 0);
-};
-
-const totalPrice = calcAll(cartItems);
-console.log(`Total Price: $${totalPrice}`);
+products.forEach((product) => {
+  let price;
+  if (product.price == null) {
+    price = "undefined";
+  } else {
+    price = product.price;
+  }
+  console.log(`Name: ${product.name}, Price: ${price}`);
+});
